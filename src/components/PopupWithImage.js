be724e029f389c7b00e6283f99856fbc28dbb0d2
MC-Genevy <115513060+Genevy@users.noreply.github.com>
2023-04-01 16:59:02 +0300

    Modified: modified placeholder area & image background

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -14,7 +14,7 @@ export default class PopupWithImage extends Popup {
     this._popupImageTitle.textContent = name;
 
     this._popupImage.onerror = () => {
-      this._popupImage.src = 'https://raw.githubusercontent.com/genevy/mesto/main/src/images/placeholder.webp';
+      this._popupImage.src = 'https://raw.githubusercontent.com/genevy/mesto/main/src/images/placeholder.svg';
       this._popupImage.alt = `${name} - картинка не загружена`;
       this._popupImageTitle.textContent = `${name} - картинка не загружена`;
     }
